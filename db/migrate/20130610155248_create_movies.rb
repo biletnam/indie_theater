912b27abe35d0d64521b81ebf8d318e8b0775934
James Pothen <jjp43@cornell.edu>
2013-06-10 12:01:49 -0400

    create movies table and model

--- db/migrate/20130610155248_create_movies.rb
+++ db/migrate/20130610155248_create_movies.rb
@@ -1,7 +1,15 @@
 class CreateMovies < ActiveRecord::Migration
-  def up
-  end
-
-  def down
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :rating
+      t.datetime :opening_date
+      t.string :starring
+      t.string :director
+      t.integer :runtime
+      t.string :released_by
+      t.string :genres
+      t.boolean :three_d
+    end
   end
 end
