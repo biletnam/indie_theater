d61311bfad6bd99a6f9d5012a920d404c884c6ea
James Pothen <jjp43@cornell.edu>
2013-06-10 13:34:19 -0400

    stop work on models, start with wireframes

--- app/models/movies.rb
+++ app/models/movies.rb
@@ -1,4 +1,4 @@
 class Movie < ActiveRecord::Base
-  attr_accessible :title, :rating, :opening_date, :starring, :director, :runtime
-                  :released_by, :genres, :three_d
+  attr_accessible :title, :rating, :opening_date, :starring, :director, 
+                  :runtime, :released_by, :genres, :three_d
 end
