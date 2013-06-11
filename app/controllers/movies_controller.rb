582cbe72599a0b078e324b4769604809ae28434c
James Pothen <jjp43@cornell.edu>
2013-06-11 09:50:53 -0400

    movies index page now self-updates based on current date

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    now = Date.today.to_s
+    @now_playing = Movie.where("opening_date < ?", now)
+    @coming_soon = Movie.where("opening_date > ?", now)
   end
 end
