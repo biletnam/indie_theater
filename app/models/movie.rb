0a76f4d7028daac9374201a1d5c680598498917d
James Pothen <jjp43@cornell.edu>
2013-06-11 10:33:07 -0400

    add posters to movies using paperclip

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,9 @@
 class Movie < ActiveRecord::Base
   attr_accessible :title, :description, :rating, :opening_date, :starring, :director, 
-                  :runtime, :released_by, :genres, :three_d, :online
+                  :runtime, :released_by, :genres, :three_d, :online, :poster
+                  
+  has_attached_file :poster, :styles => { :small => "78x118", :medium => "198x298" }, 
+                    :default_url => "/images/:style/missing.png"
   
   scope :online, where(:online => true)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130611141159) do
+ActiveRecord::Schema.define(:version => 20130611142845) do
 
   create_table "movies", :force => true do |t|
     t.string   "title"
@@ -24,9 +24,13 @@ ActiveRecord::Schema.define(:version => 20130611141159) do
     t.string   "released_by"
     t.string   "genres"
     t.boolean  "three_d"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
     t.boolean  "online"
+    t.string   "poster_file_name"
+    t.string   "poster_content_type"
+    t.integer  "poster_file_size"
+    t.datetime "poster_updated_at"
   end
 
 end
