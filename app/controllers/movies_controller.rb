d01a21a60900736c7ac4856fbd2427699deb96d3
James Pothen <jjp43@cornell.edu>
2013-06-11 13:45:55 -0400

    able to add new movies with form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,4 +11,18 @@ class MoviesController < ApplicationController
     @now_playing = Movie.where("opening_date < ?", now).online
     @coming_soon = Movie.where("opening_date > ?", now).online
   end
+  
+  def new
+    @movie = Movie.new
+  end
+  
+  def create
+    @movie = Movie.new(params[:movie])
+    
+    if @movie.save!
+      redirect_to movie_url(@movie)
+    else
+      render :new
+    end
+  end
 end
