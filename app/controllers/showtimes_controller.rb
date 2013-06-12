66da18d719007534720520e5cb68b6751b585215
James Pothen <jjp43@cornell.edu>
2013-06-12 10:26:39 -0400

    now display only movies now playing, and showtimes only for today. Still need to fix timezone problem

--- app/controllers/showtimes_controller.rb
+++ app/controllers/showtimes_controller.rb
@@ -1,6 +1,8 @@
 class ShowtimesController < ApplicationController
   def index
     @today = DateTime.now
+    now = Date.today.to_s
+    @now_playing = Movie.where("opening_date < ?", now).online
     @showtimes = Showtime.where('time BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
   end
 end
--- app/models/showtime.rb
+++ app/models/showtime.rb
@@ -1,5 +1,5 @@
 class Showtime < ActiveRecord::Base
-  attr_accessible :screening
+  attr_accessible :time
   
   has_many :movie_showtimes
   has_many :movies, :through => :movie_showtimes
