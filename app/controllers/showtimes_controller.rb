d7ef6d7847793d596b31f982c6927c0431ff42f3
James Pothen <jjp43@cornell.edu>
2013-06-12 14:23:13 -0400

    fix showtimes controller to display movies that have not yet opened

--- app/controllers/showtimes_controller.rb
+++ app/controllers/showtimes_controller.rb
@@ -4,7 +4,7 @@ class ShowtimesController < ApplicationController
     now = Date.today.to_s
     @day = @today + params[:day].to_i
 
-    @now_playing = Movie.where("opening_date < ?", now).online
+    @now_playing = Movie.where("opening_date < ?", @day).online
     @showtimes = Showtime.where('time BETWEEN ? AND ?', @day.beginning_of_day, @day.end_of_day).all
   end
 end
