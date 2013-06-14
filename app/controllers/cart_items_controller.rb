80a460e6562eb96e3cd0d28c759b2a6e943886dd
James Pothen <jjp43@cornell.edu>
2013-06-13 21:48:53 -0400

    fixing it so you can only put a valid number of seats into an order

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,10 +1,13 @@
 class CartItemsController < ApplicationController
   
-  # before_filter :authenticate_customer!, :only => [:show]
+  before_filter :authenticate_customer!
   
   def create
     @cart_item = CartItem.new(params[:cart_item])
     @cart_item.customer_id = current_customer.id
+    @showtime = Showtime.find(@cart_item.showtime_id)
+    
+    # @showtime.tickets.size + @cart_item.quantity <= @showtime.screen.seats
     
     if @cart_item.save
       redirect_to customer_url(current_customer)
