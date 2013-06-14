a505f711d6878d4b5ced31e44ba65e307abf65a1
James Pothen <jjp43@cornell.edu>
2013-06-14 11:28:53 -0400

    fix problem with movies that open today

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,14 +4,14 @@ class MoviesController < ApplicationController
   
   def index
     now = Date.today.to_s
-    @now_playing = Movie.where("opening_date < ?", now).online
+    @now_playing = Movie.where("opening_date <= ?", now).online
     @coming_soon = Movie.where("opening_date > ?", now).online
   end
   
   def show
     @movie = Movie.find(params[:id])
     now = Date.today.to_s
-    @now_playing = Movie.where("opening_date < ?", now).online
+    @now_playing = Movie.where("opening_date <= ?", now).online
     @coming_soon = Movie.where("opening_date > ?", now).online
   end
   
--- app/controllers/showtimes_controller.rb
+++ app/controllers/showtimes_controller.rb
@@ -8,7 +8,7 @@ class ShowtimesController < ApplicationController
     @day = @today + params[:day].to_i
     @offset = params[:day].to_i
     
-    @now_playing = Movie.where("opening_date < ?", @day).online
+    @now_playing = Movie.where("opening_date <= ?", @day).online
     @showtimes = Showtime.where('time BETWEEN ? AND ?', @day.beginning_of_day, @day.end_of_day).all
   end
   
