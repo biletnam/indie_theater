71647c5b56ff7458d8782e92b3e9696f0ced10fa
James Pothen <jjp43@cornell.edu>
2013-06-14 12:15:07 -0400

    fix error where you could only buy up to 1 less than number of seats for a showtime

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -7,7 +7,7 @@ class CartItemsController < ApplicationController
     @cart_item.customer_id = current_customer.id
     @showtime = Showtime.find(@cart_item.showtime_id)
     
-    if @showtime.tickets.size + @cart_item.quantity >= @showtime.screen.seats
+    if @showtime.tickets.size + @cart_item.quantity > @showtime.screen.seats
       redirect_to showtime_url(@showtime)
     elsif @cart_item.save 
       redirect_to customer_url(current_customer)
