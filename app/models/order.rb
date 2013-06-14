90f5c1ee510bc6d5e0f7e06f3511dd8b8653e819
James Pothen <jjp43@cornell.edu>
2013-06-14 12:01:57 -0400

    destroy tickets when an order is cancelled

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ActiveRecord::Base
   attr_accessible :customer_id, :ticket_ids
     
   belongs_to :customer
-  has_many :tickets
+  has_many :tickets, :dependent => :destroy
   
   accepts_nested_attributes_for :tickets
 end
