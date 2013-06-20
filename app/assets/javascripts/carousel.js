5bf94dfd9cda91863a550e270b9745756082836d
James Pothen <jjp43@cornell.edu>
2013-06-20 13:53:55 -0400

    moved carousel javascript into separate file

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,6 +1,6 @@
 var Carousel = (function(){
 	
-	var delay = 5000;
+	var delay = 6000;
 	var carousel = $("#carousel");
 	var itemsHolder = carousel.find("> ul");
 	var items = itemsHolder.find("> li");
@@ -8,10 +8,21 @@ var Carousel = (function(){
 	var currentItem = -1;
 	var timerId;
 	
+  var firstButton = $("#carousel-nav > .first");
+  var secondButton = $("#carousel-nav > .second");	
+	var thirdButton = $("#carousel-nav > .third");
+	
 	var init = function(){
 		itemsHolder.css({"width": (items.length * itemWidth) + "px"});
+		bind();
 		start();
 	};
+	
+  var bind = function(){
+    firstButton.on("click", first);
+    secondButton.on("click", second);
+    thirdButton.on("click", third);
+  }
 	
 	var start = function(){
 		loop();
@@ -49,11 +60,47 @@ var Carousel = (function(){
 	}
 	
 	var move = function(){
-		var html = (currentItem + 1) + " / " + items.length;
-		// numberLabel.html(html);
-		
 		var leftPosition = currentItem * itemWidth * -1;
 		itemsHolder.stop().animate({"left": leftPosition + "px"}, 1000);
+
+		animateDots();
+	}
+	
+	var animateDots = function(){
+		firstButton.html("&#9679;");
+		secondButton.html("&#9679;");
+		thirdButton.html("&#9679;");
+		
+		if(currentItem == 0){
+			firstButton.html("&#9675;");
+		}
+		else if(currentItem == 1){
+			secondButton.html("&#9675;");
+		}
+		else{
+			thirdButton.html("&#9675;");
+		}
+	}
+	
+	var first = function(){
+		currentItem = 0;
+
+		animateDots();
+		move();
+	}
+	
+	var second = function(){
+		currentItem = 1;
+		
+		animateDots();
+		move();
+	}
+	
+	var third = function(){
+		currentItem = 2;
+		
+		animateDots();
+		move();
 	}
 	
 	return {
@@ -64,3 +111,7 @@ var Carousel = (function(){
 		stop: stop
 	}
 })();
+
+$(document).ready(function(){
+	Carousel.init();
+});
